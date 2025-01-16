b946e02edb57c23d1ead229da28bac6582eb3b76
Fyvwfdydch71ayc <guruji123k+28@gmail.com>
2025-01-16 09:42:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 import os  # Import os module to fetch environment variables
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, ChatJoinRequestHandler
 from web_server import start_web_server  # Import the web server function
-from script1 import start, approve, detail, send_cv, handle_message, ADMIN_ID  # Import the updated functions including ADMIN_ID
+from script1 import download_logo, get_custom_caption, add_logo_to_image, handle_media, start  # Import the updated functions including ADMIN_ID
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -18,24 +18,11 @@ async def run_bot() -> None:
     
     app = ApplicationBuilder().token(bot_token).build()  # Use the bot token
 
-    # Add command and message handlers
-    start_handler = CommandHandler("start", start)
-    app.add_handler(start_handler)
+    #    # Add handlers
+    application.add_handler(CommandHandler("start", start))
 
-    # Add handler for detailed information about joined chats (only for the admin)
-    detail_handler = CommandHandler("detail", detail)
-    app.add_handler(detail_handler)
-
-    # Add handler for sending CSV file of user IDs (only for the admin)
-    id_handler = CommandHandler("id", send_cv)
-    app.add_handler(id_handler)
-
-    # MessageHandler for all types (video, sticker, gif, etc.)
-    all_handler = MessageHandler(filters.ALL, handle_message)
-    app.add_handler(all_handler)
-
-    # Handle chat join requests
-    app.add_handler(ChatJoinRequestHandler(approve))
+    # Use filters.ALL to capture all types of messages
+    application.add_handler(MessageHandler(filters.ALL, handle_media))
 
     await app.run_polling()
 
