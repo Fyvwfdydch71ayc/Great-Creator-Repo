7c81fbe04cbb2a704ce47e84b236a5175cf7d025
Fyvwfdydch71ayc <guruji123k+28@gmail.com>
2025-01-22 21:16:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 from script1 import generate_telegram_link, start, create_link, username, message, send_long_message, show_create_link_button, cancel  # Corrected import statement
 from web_server import start_web_server  # Import the web server function
 
-# Constants for ConversationHandler (Ensure these are defined in your script)
-USERNAME, MESSAGE = range(2)
+
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
