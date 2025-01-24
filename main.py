1c80c152bf644a6692331f592bd29b3648e26161
Fyvwfdydch71ayc <guruji123k+28@gmail.com>
2025-01-24 14:08:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 import os  # Import the os module to access environment variables
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ConversationHandler, ContextTypes, CallbackContext, Application# Add ConversationHandler import
-from script1 import download_logo, get_custom_caption, add_logo_to_image, handle_media, start # Corrected import statement
+from script1 import handle_media, start # Corrected import statement
 from web_server import start_web_server  # Import the web server function
 
 
@@ -21,12 +21,10 @@ async def run_bot() -> None:
 
     app = ApplicationBuilder().token(bot_token).build()  # Use the token
 
-    # Add handlers
-    app.add_handler(CommandHandler("start", start))  # Corrected this line
 
+    app.add_handler(CommandHandler("start", start))
     # Use filters.ALL to capture all types of messages
     app.add_handler(MessageHandler(filters.ALL, handle_media))
-
     # Run the bot using asyncio
     await app.run_polling()
 
